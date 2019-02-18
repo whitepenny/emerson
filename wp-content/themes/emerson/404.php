67d5f375d7f71694dc24bfe98ec2a2ce27d8069d
Scott Revell <srevell@whitepenny.com>
2019-02-18 16:28:32 -0500

    404 page verbiage

--- wp-content/themes/emerson/404.php
+++ wp-content/themes/emerson/404.php
@@ -4,6 +4,15 @@ get_header();
 
 ?>
 <?php get_template_part( 'layouts/page_header', '404' ); ?>
+<div class="fs-row page_main">
+  <div class="fs-cell fs-lg-last fs-lg-justify-end fs-lg-4 page_sidebar">
+  </div>
+  <div class="fs-cell fs-lg-8 page_container">
+    <div class="page_content">
+      <p>The page you are attempting to access may have been moved or deleted. We apologize for the inconvenience.</p>
+    </div>
+  </div>
+</div>
 <?php
 
 get_footer();
